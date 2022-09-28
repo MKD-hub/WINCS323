ed8cedbd7d51240be42a762620d3bffe2d712fea
MK <57914774+MKD-hub@users.noreply.github.com>
2022-09-28 14:59:38 +0300

    Review table data class

---
+++ Data/Reviews.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProject.Data
+{
+    public class Reviews
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int CommentId { get; set; }
+
+        public string ReviewBookName { get; set; }
+
+        public string ReviewTitle { get; set; }
+
+        public string ReviewBody { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        public string ReviewImage { get; set; }
+
+
+    }
+}
