aee4dca3229f51c5ccbaddeb116a9ffbf353fa60
MK <57914774+MKD-hub@users.noreply.github.com>
2022-09-28 14:58:15 +0300

    Basic skeleton

--- Data/BookReviewContext.cs
+++ Data/BookReviewContext.cs
@@ -1,1 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProject.Model;
 
+namespace WebProject.Data
+{
+    public class BookReviewContext : IdentityDbContext<ApplicationUser>
+    {
+        public BookReviewContext(DbContextOptions<BookReviewContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Reviews> Reviews { get; set; }
+    }
+
+
+}
