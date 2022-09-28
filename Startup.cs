0adb97d65a40904ba70eed007544b1cae7f30d86
MK <57914774+MKD-hub@users.noreply.github.com>
2022-09-28 14:54:05 +0300

    Basic skeleton of program

---
+++ Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProject.Data;
+using WebProject.Model;
+using WebProject.Repository;
+
+namespace WebProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<BookReviewContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BookReviewsDB")));
+
+            /*Whenever the above line executes it creates a session with the DB during the lifetime of the HTTP request invoking it. When it's invoked it creates a DBContext
+             of type BookReviewContext and pass the options provided establish a session with the DB.*/
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().
+                AddEntityFrameworkStores<BookReviewContext>().
+                AddDefaultTokenProviders();
+
+            services.AddControllers();
+            services.AddTransient<IReviewsRepository, ReviewsRepository>();
+            services.AddAutoMapper(typeof(Startup));
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
